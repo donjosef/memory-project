0a0caf2059583aba3ec0cb668668967864369f9f
donjoe <giumontanaro@gmail.com>
2018-03-09 21:25:52 +0100

    Adding function to shuffle the cards

--- js/app.js
+++ js/app.js
@@ -1,29 +1,22 @@
-/*
- * Create a list that holds all of your cards
- */
+const cards = document.querySelectorAll(".card");
+const icons = document.querySelectorAll(".deck .fa");
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+function shuffle() {
+  let random = Math.floor(Math.random() * cards.length);
+  for(let i = 0; i < cards.length; i++) {
+    cards[i].appendChild(icons[random]);
+    random++;
+    if(random === cards.length) {
+        random = 0;
     }
-
-    return array;
+  }
 }
+  shuffle();
+
+
+
+
 
 
 /*
